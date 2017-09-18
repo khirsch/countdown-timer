b3763ce9315612d0af029ffac30a83392ffe6060
Katrina Hirsch <katrina.hirsch@gmail.com>
2017-09-18 15:10:08 -0700

    add simple timer function

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
-// scripts go here!
+function timer(seconds) {
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    if (secondsLeft <= 0) {
+      return;
+    }
+    console.log(secondsLeft);
+  }, 1000);
+}
