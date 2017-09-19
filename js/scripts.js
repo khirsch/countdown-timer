9b9cc7c87657fb344e0d613f1d27b1d00cb9c426
Katrina Hirsch <katrina.hirsch@gmail.com>
2017-09-18 21:04:53 -0700

    add rule to clear interval if timer is reset

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ const endTime = document.querySelector('.display__end-time');
 const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
+  clearInterval(countdown);
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
