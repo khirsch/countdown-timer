cb2f20bf8c59a6b2717b43c5809c40e03d7da083
Katrina Hirsch <katrina.hirsch@gmail.com>
2017-09-18 17:47:51 -0700

    create basic function to display time remaining

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,17 @@ let countdown;
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
     if (secondsLeft <= 0) {
       clearInterval(countdown);
       return;
     }
-    console.log(secondsLeft);
+    displayTimeLeft(secondsLeft);
   }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  console.log(seconds);
 }
